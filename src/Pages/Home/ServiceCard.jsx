1a5ac50f830fa05a2fcba212ed64bfd64053d80c
devmashum <dev.mashum@gmail.com>
2023-12-10 00:05:00 +0100

    service area card updated

--- src/Pages/Home/ServiceCard.jsx
+++ src/Pages/Home/ServiceCard.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BiArrowToRight } from "react-icons/bi";
 
 const ServiceCard = ({ service }) => {
 
     const { _id, title, img, price } = service;
 
     return (
+      <>
+          <Link to={`/book/${_id}`}> 
         <div className="card w-96 mx-auto bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-                <img src={img} alt="Shoes" className="rounded-xl" />
+                <img src={img} alt="Shoes" className="rounded-xl w-[350px] h-[200px]" />
             </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">{title}</h2>
-                <p>Price: $ {price}</p>
+            <div className="card-body">
+                <h2 className="card-title text-[25px] font-bold text-[#444]">{title}</h2>
+             <div className='flex justify-evenly'>
+             <p className='text-[#FF3811] text-xl font-semibold'>Price: $ {price}</p>
+             <p className='text-[#FF3811] text-2xl font-semibold'><BiArrowToRight /></p>
+             </div>
                 <div className="card-actions">
-                    <Link to={`/book/${_id}`}> <button className="btn btn-primary">Book Now</button></Link>
+                  
                 </div>
             </div>
         </div>
+        </Link>
+      </>
     );
 };
 
--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -2,38 +2,50 @@ import React, { useEffect, useState } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
+  // State for sorting services
+  const [asc, setAsc] = useState(true);
 
-    // to show the low price first 
+  // State to store services data
+  const [services, setServices] = useState([]);
 
-    const [asc, setAsc] = useState(true);
+  // Fetch services data on component mount or when sorting order changes
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/services?sort=${asc ? 'asc' : 'desc'}`);
+        const data = await response.json();
+        setServices(data);
+      } catch (error) {
+        console.error('Error fetching services:', error);
+      }
+    };
 
-    const [Services, setServices] = useState([]);
+    fetchServices();
+  }, [asc]);
 
-    useEffect(() => {
-        fetch(`http://localhost:3000/services?sort=${asc ? 'asc' : 'desc'}`)
-            .then(res => res.json())
-            .then(data => setServices(data));
-    }, [asc])
+  return (
+    <div className='ml-12'>
+      <div className='lg:text-center'>
+        <p className='text-[#FF3811] text-xl font-bold mt-5'>Service</p>
+        <h3 className="text-[45px] text-[#151515] font-bold">Our Service Area</h3>
+        <p className='text-[16px] text-[#737373] md-[50px]'>
+          Keep your vehicle in peak condition with our routine maintenance services. From oil changes and fluid checks to tire rotations and brake inspections, our skilled technicians perform thorough assessments to prevent potential issues and extend the life of your vehicle.
+        </p>
 
-
-
-    return (
-        <div>
-            <div className='lg:text-center ml-12'>
-                <h3 className="text-2xl font-bold text-orange-600">Service</h3>
-                <h3 className="text-4xl ">Our Service Area</h3>
-                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                <option onClick={() => setAsc(!asc)} className='btn btn-secondary'>{asc ? 'Price High Low' : 'Price: Low to High'}</option>
-
-                <button onClick={() => setAsc(!asc)} className='btn btn-secondary'>{asc ? 'Price High Low' : 'Price: Low to High '}</button>
-            </div>
-            <div className='grid lg:grid-cols-3 md:grid-col-3 gap-5'>
-                {
-                    Services.map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)
-                }
-            </div>
+        <div className='hidden'>
+          <button onClick={() => setAsc(!asc)} className='btn btn-secondary'>
+            {asc ? 'Price High Low' : 'Price: Low to High'}
+          </button>
         </div>
-    );
+      </div>
+
+      <div className='grid lg:grid-cols-3 md:grid-col-3 gap-5'>
+        {services.map(service => (
+          <ServiceCard key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
